893a5918bb592bdb63166dd47b83d78a776cccf5
Kunal-Khairnar-05 <kunalkhairnar2005@gmail.com>
2022-02-03 14:27:25 +0530

    Adding basic programs
    Adding basic programs

---
+++ Basic-Programs/2nd_file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+int i;
+scanf("%d",&i);
+
+do{
+    printf("%d \n",i);
+    i++;
+
+}
+while(i<=153);
+
+    return 0;
+}
---
+++ Basic-Programs/addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int sum(int a,int b){
+    int result;
+    result= a+b;
+   return result;
+}
+int main()
+{
+    
+    printf("%d",sum(2,4));
+    
+return 0;
+}
